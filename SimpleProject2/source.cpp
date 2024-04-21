9a4973723599bd5073a0622cd501d979446855c6
ValentynNVS <novosidlak3@gmail.com>
2024-04-20 20:02:46 -0400

    Added a function comment to each function to explain how they will work as well as their parameters and return values.

--- SimpleProject2/source.cpp
+++ SimpleProject2/source.cpp
@@ -36,6 +36,14 @@ int main(void) {
 
 }
 
+//
+// FUNCTION : calculateCostOfRoom
+// DESCRIPTION : This fucntion counts the cost of stay in a room
+// PARAMETERS : double roomRates: price for a night
+//				int checkIn: check in day as an index of an array
+//				int checkOut: check out day as an index of an array
+// RETURNS : return costOfRoom: returns the cost of living for a 1 person in a period that he stayed.
+//
 double calculateCostOfRoom(const double[], int, int) {
 
 
@@ -43,9 +51,31 @@ double calculateCostOfRoom(const double[], int, int) {
 
 }
 
+
+//
+// FUNCTION : indexTheDay
+// DESCRIPTION : This fucntion compares user input to see if it correct and matches a week day
+//				 Then this program checks if the weekday is allowed to be used
+//				 or if the string is empty
+// PARAMETERS : char dayEntered: user input  in a form of a string
+// RETURNS : return kErrorValue: in case that string enter by the user does not match anything
+//			 return 1,2,3,4,5,6: respectivly to the weekdays
+//
 int indexTheDay(char[]) {
 
 	return 0;
 }
 
-void takeOffEscapeSequence(char[]);
+
+//
+// FUNCTION : takeOffEscapeSequence
+// DESCRIPTION : This fucntion takes away the "\n" which fgets function automaticly adds everytime.
+//				 This is crucial for a proper comperisson of strings
+// PARAMETERS : char userInput[]: string that needs to be modified
+// RETURNS : void: does not return anything
+//
+void takeOffEscapeSequence(char[]) {
+
+
+
+}
