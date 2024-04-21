bdc26040b55a6c14247092dc37a3107ca5fce0eb
ValentynNVS <novosidlak3@gmail.com>
2024-04-20 20:00:06 -0400

    Created a file header, imported the libraries I will use in my project. And briefly described the idea of my code.

---
+++ SimpleProject2/source.cpp
@@ -0,0 +1,20 @@
+/*
+* FILE : source.cpp
+* PROGRAMMER : Valentyn Novosydliuk
+* FIRST VERSION : 04/20/2024
+* DESCRIPTION :
+* This C program calculates hotel bills for meeting attendees based on check-in and check-out dates,
+* ensuring attendees arrive no later than Thursday and depart no earlier than Thursday.
+* It employs modular functions for input validation, cost calculation, and error handling, offering a streamlined interface for managing hotel bookings.
+*/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(void) {
+
+
+
+
+
+}
